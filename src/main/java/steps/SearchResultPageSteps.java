a6e1fc7cd898e9ea1572a031dda26ac8d7438d4c
Patryk Karbowy <patryk.karbowy11@gmail.com>
2024-07-12 17:51:06 +0200

    Added date in file formats, created method to return current date as a String

--- src/main/java/steps/SearchResultPageSteps.java
+++ src/main/java/steps/SearchResultPageSteps.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import pages.BasicPage;
 import pages.SearchResultPage;
 import utils.ExcelWriter;
+import utils.UtilsMethods;
 
 public class SearchResultPageSteps extends BasicPage {
     private SearchResultPage searchResultPage = new SearchResultPage(driver);
@@ -21,8 +22,12 @@ public class SearchResultPageSteps extends BasicPage {
     }
     public void saveAllItemTextObjectsToExcel(int numberOfProductsToSave) throws InterruptedException{
         ExcelWriter excelWriter = new ExcelWriter((short)12, true);
-        excelWriter.saveSearchResultToExcelFile("OLX_Search_Result","Search_Result",searchResultPage.getValuesFromSearchResult(numberOfProductsToSave));
-        logger.info("Saved Excel with found products in: /OLX_Search_Result.xlsx");
+        String currentDate = UtilsMethods.getCurrentDate();
+        String fileName = "OLX_Search_Result_" + currentDate;
+        String sheetName = "Search_Result_" + currentDate;
+        String fileNameFormatted = fileName + ".xlsx";
+        excelWriter.saveSearchResultToExcelFile(fileName,sheetName,searchResultPage.getValuesFromSearchResult(numberOfProductsToSave));
+        logger.info("Saved Excel with found products in: {}",fileNameFormatted);
     }
 
 }
